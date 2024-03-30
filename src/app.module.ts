004f6bd311ff41390eef0bc71b93830dd3426132
christian-bayata <franksagie1@gmail.com>
2024-03-30 18:07:59 +0100

    Setup hotel reservations file interceptor and app module files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { existsSync, mkdirSync } from 'fs';
+import * as path from 'path';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+      expandVariables: true,
+    }),
+    // HotelReservationMicroserviceModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  async onModuleInit(): Promise<void> {
+    if (!existsSync(path.join(__dirname, '..', 'logs'))) {
+      mkdirSync(path.join(__dirname, '..', 'logs'));
+    }
+    return;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
+import { HotelReservationMsLogger } from './common/logger.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('HotelReservationMicroservice');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RABBITMQ_URL],
+        queue: 'hotel_reservation_microservice',
+        queueOptions: {
+          durable: true,
+        },
+      },
+    },
+  );
+
+  app.useGlobalInterceptors(new HotelReservationMsLogger());
+  await app
+    .listen()
+    .finally(() =>
+      logger.log(
+        `Hotel Reservation Microservice: EventBus:${process.env.RABBITMQ_URL}`,
+      ),
+    );
 }
 bootstrap();
